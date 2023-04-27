375b8ccd25ce0835c3b880047f70864f2aa1411b
Coalus <im-coalus@proton.me>
2023-04-27 19:14:44 +0300

    Fix telegram id filtering

--- src/handlers/whitelist.ts
+++ src/handlers/whitelist.ts
@@ -4,9 +4,9 @@ import { User } from '@/db';
 export const addUserToWhitelist = async (ctx: Context) => {
   const user = new User();
 
-  const userId = ctx.message?.text?.split(' ')[1];
-
-  if (userId && userId.length === 9) {
+  const userId = ctx.message!.text!.split(' ')[1];
+  const pattern = /^\d+$/;
+  if (pattern.test(userId)) {
     user.user_id = Number(userId);
 
     await user.save();
@@ -22,9 +22,10 @@ export const addUserToWhitelist = async (ctx: Context) => {
 };
 
 export const deleteUserFromWhitelist = async (ctx: Context) => {
-  const userId = ctx.message?.text?.split(' ')[1];
+  const userId = ctx.message!.text!.split(' ')[1];
+  const pattern = /^\d+$/;
 
-  if (userId && userId.length === 9) {
+  if (pattern.test(userId)) {
     const user = await User.findOneBy({ user_id: Number(userId) });
 
     if (user === null) {
@@ -50,11 +51,10 @@ export const showWhitelist = async (ctx: Context) => {
     await ctx.reply('Users whitelist is empty');
     return;
   }
-
   await ctx.reply(
-    `*Users whitelist:*\n${users
-      .map((user, index) => `${index + 1}. ${user.user_id}`)
+    `<b>Users whitelist:</b>\n${users
+      .map((user, index) => `${index + 1}. <a href="tg://user?id=${user.user_id}">${user.user_id}</a>`)
       .join('\n')}`,
-    { parse_mode: 'Markdown' }
+    { parse_mode: 'HTML' }
   );
 };
