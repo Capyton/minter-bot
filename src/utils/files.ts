99bb082ff22d6834692a7f65d118dd73f9df5957
krau5 <apvbd17@gmail.com>
2023-05-03 21:14:57 +0200

    Get file util update

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,5 +1,6 @@
-import * as fs from 'fs';
 import path from 'path';
+import { escape } from 'querystring';
+import download from 'download';
 import { Context } from '@/types';
 
 type FileType = 'document' | 'photo';
@@ -13,35 +14,36 @@ export const getFileId = (ctx: Context, type: FileType): string => {
     }
   }
 
-  const photos = ctx.update.message?.photo;
+  if (type === 'photo' && ctx.message?.photo) {
+    const photo = ctx.message.photo.pop();
 
-  if (photos) {
-    console.log(photos);
-    return photos[0].file_id;
+    return photo!.file_id;
   }
 
   return '';
 };
+
+const getFileUrl = (filePath: string) =>
+  `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${escape(
+    filePath
+  )}`;
 
 export const downloadFile = async (
   ctx: Context,
   type: FileType,
   filename: string
 ): Promise<string> => {
+  const pathname = path.resolve(`${__dirname}/../assets/`);
+
   const fileId = getFileId(ctx, type);
   const file = await ctx.api.getFile(fileId);
 
-  if (typeof file.download !== 'function') {
-    throw new Error('This file is already loaded.');
-  }
+  if (file.file_path) {
+    const url = getFileUrl(file.file_path);
 
-  const pathname = await file.download();
-
-  if (pathname) {
-    fs.copyFileSync(
-      pathname,
-      path.resolve(`${__dirname}/../assets/${filename}`)
-    );
+    await download(url, pathname, { filename: filename });
+  } else {
+    throw new Error('File path does not exist');
   }
 
   return pathname;
